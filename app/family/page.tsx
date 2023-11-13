fd03d1683b90d0de6d0e50cce327d680b012050b
Your Name <root@Ethan.localdomain>
2023-11-12 18:01:19 -0800

    first commit

---
+++ app/family/page.tsx
@@ -0,0 +1,14 @@
+export default function Family() {
+  return (
+    <div>
+      <h1>Meet The Fam!</h1>
+      <h2>Amaya Navarro</h2>
+      <h2>Jazlin Navarro</h2>
+      <h2>Michael Navarro</h2>
+      <h2>Julia Ruiz</h2>
+      <h2>Louie Gonzales</h2>
+      <h2>Andrew Davis</h2>
+      <h2>Andrew Mohler</h2>
+    </div>
+  )
+}
---
+++ app/vlogs/page.tsx
@@ -0,0 +1,5 @@
+export default function Vlogs() {
+  return (
+    <h1>Vlogs</h1>
+  )
+}
